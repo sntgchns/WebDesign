baf65127196b2b427d5a07461018048bd1c9075e
Santiago Soñora <santiagochinas@hotmail.com>
2023-04-30 19:20:21 -0300

    new

---
+++ Inmersive_effect/assets/js/scripts.js
@@ -0,0 +1,480 @@
+// Utility function
+function Util () {};
+
+/* 
+	class manipulation functions
+*/
+Util.hasClass = function(el, className) {
+	if (el.classList) return el.classList.contains(className);
+	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+};
+
+Util.addClass = function(el, className) {
+	var classList = className.split(' ');
+ 	if (el.classList) el.classList.add(classList[0]);
+ 	else if (!Util.hasClass(el, classList[0])) el.className += " " + classList[0];
+ 	if (classList.length > 1) Util.addClass(el, classList.slice(1).join(' '));
+};
+
+Util.removeClass = function(el, className) {
+	var classList = className.split(' ');
+	if (el.classList) el.classList.remove(classList[0]);	
+	else if(Util.hasClass(el, classList[0])) {
+		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+		el.className=el.className.replace(reg, ' ');
+	}
+	if (classList.length > 1) Util.removeClass(el, classList.slice(1).join(' '));
+};
+
+Util.toggleClass = function(el, className, bool) {
+	if(bool) Util.addClass(el, className);
+	else Util.removeClass(el, className);
+};
+
+Util.setAttributes = function(el, attrs) {
+  for(var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+};
+
+/* 
+  DOM manipulation
+*/
+Util.getChildrenByClassName = function(el, className) {
+  var children = el.children,
+    childrenByClass = [];
+  for (var i = 0; i < el.children.length; i++) {
+    if (Util.hasClass(el.children[i], className)) childrenByClass.push(el.children[i]);
+  }
+  return childrenByClass;
+};
+
+Util.is = function(elem, selector) {
+  if(selector.nodeType){
+    return elem === selector;
+  }
+
+  var qa = (typeof(selector) === 'string' ? document.querySelectorAll(selector) : selector),
+    length = qa.length,
+    returnArr = [];
+
+  while(length--){
+    if(qa[length] === elem){
+      return true;
+    }
+  }
+
+  return false;
+};
+
+/* 
+	Animate height of an element
+*/
+Util.setHeight = function(start, to, element, duration, cb) {
+	var change = to - start,
+	    currentTime = null;
+
+  var animateHeight = function(timestamp){  
+    if (!currentTime) currentTime = timestamp;         
+    var progress = timestamp - currentTime;
+    var val = parseInt((progress/duration)*change + start);
+    element.style.height = val+"px";
+    if(progress < duration) {
+        window.requestAnimationFrame(animateHeight);
+    } else {
+    	cb();
+    }
+  };
+  
+  //set the height of the element before starting animation -> fix bug on Safari
+  element.style.height = start+"px";
+  window.requestAnimationFrame(animateHeight);
+};
+
+/* 
+	Smooth Scroll
+*/
+
+Util.scrollTo = function(final, duration, cb, scrollEl) {
+  var element = scrollEl || window;
+  var start = element.scrollTop || document.documentElement.scrollTop,
+    currentTime = null;
+
+  if(!scrollEl) start = window.scrollY || document.documentElement.scrollTop;
+      
+  var animateScroll = function(timestamp){
+  	if (!currentTime) currentTime = timestamp;        
+    var progress = timestamp - currentTime;
+    if(progress > duration) progress = duration;
+    var val = Math.easeInOutQuad(progress, start, final-start, duration);
+    element.scrollTo(0, val);
+    if(progress < duration) {
+        window.requestAnimationFrame(animateScroll);
+    } else {
+      cb && cb();
+    }
+  };
+
+  window.requestAnimationFrame(animateScroll);
+};
+
+/* 
+  Focus utility classes
+*/
+
+//Move focus to an element
+Util.moveFocus = function (element) {
+  if( !element ) element = document.getElementsByTagName("body")[0];
+  element.focus();
+  if (document.activeElement !== element) {
+    element.setAttribute('tabindex','-1');
+    element.focus();
+  }
+};
+
+/* 
+  Misc
+*/
+
+Util.getIndexInArray = function(array, el) {
+  return Array.prototype.indexOf.call(array, el);
+};
+
+Util.cssSupports = function(property, value) {
+  if('CSS' in window) {
+    return CSS.supports(property, value);
+  } else {
+    var jsProperty = property.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase();});
+    return jsProperty in document.body.style;
+  }
+};
+
+// merge a set of user options into plugin defaults
+// https://gomakethings.com/vanilla-javascript-version-of-jquery-extend/
+Util.extend = function() {
+  // Variables
+  var extended = {};
+  var deep = false;
+  var i = 0;
+  var length = arguments.length;
+
+  // Check if a deep merge
+  if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
+    deep = arguments[0];
+    i++;
+  }
+
+  // Merge the object into the extended object
+  var merge = function (obj) {
+    for ( var prop in obj ) {
+      if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+        // If deep merge and property is an object, merge properties
+        if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
+          extended[prop] = extend( true, extended[prop], obj[prop] );
+        } else {
+          extended[prop] = obj[prop];
+        }
+      }
+    }
+  };
+
+  // Loop through each object and conduct a merge
+  for ( ; i < length; i++ ) {
+    var obj = arguments[i];
+    merge(obj);
+  }
+
+  return extended;
+};
+
+// Check if Reduced Motion is enabled
+Util.osHasReducedMotion = function() {
+  if(!window.matchMedia) return false;
+  var matchMediaObj = window.matchMedia('(prefers-reduced-motion: reduce)');
+  if(matchMediaObj) return matchMediaObj.matches;
+  return false; // return false if not supported
+}; 
+
+/* 
+	Polyfills
+*/
+//Closest() method
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+	Element.prototype.closest = function(s) {
+		var el = this;
+		if (!document.documentElement.contains(el)) return null;
+		do {
+			if (el.matches(s)) return el;
+			el = el.parentElement || el.parentNode;
+		} while (el !== null && el.nodeType === 1); 
+		return null;
+	};
+}
+
+//Custom Event() constructor
+if ( typeof window.CustomEvent !== "function" ) {
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+}
+
+/* 
+	Animation curves
+*/
+Math.easeInOutQuad = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t + b;
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+};
+
+Math.easeInQuart = function (t, b, c, d) {
+	t /= d;
+	return c*t*t*t*t + b;
+};
+
+Math.easeOutQuart = function (t, b, c, d) { 
+  t /= d;
+	t--;
+	return -c * (t*t*t*t - 1) + b;
+};
+
+Math.easeInOutQuart = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t*t + b;
+	t -= 2;
+	return -c/2 * (t*t*t*t - 2) + b;
+};
+
+Math.easeOutElastic = function (t, b, c, d) {
+  var s=1.70158;var p=d*0.7;var a=c;
+  if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+  if (a < Math.abs(c)) { a=c; var s=p/4; }
+  else var s = p/(2*Math.PI) * Math.asin (c/a);
+  return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+};
+
+
+/* JS Utility Classes */
+(function() {
+  // make focus ring visible only for keyboard navigation (i.e., tab key) 
+  var focusTab = document.getElementsByClassName('js-tab-focus');
+  function detectClick() {
+    if(focusTab.length > 0) {
+      resetFocusTabs(false);
+      window.addEventListener('keydown', detectTab);
+    }
+    window.removeEventListener('mousedown', detectClick);
+  };
+
+  function detectTab(event) {
+    if(event.keyCode !== 9) return;
+    resetFocusTabs(true);
+    window.removeEventListener('keydown', detectTab);
+    window.addEventListener('mousedown', detectClick);
+  };
+
+  function resetFocusTabs(bool) {
+    var outlineStyle = bool ? '' : 'none';
+    for(var i = 0; i < focusTab.length; i++) {
+      focusTab[i].style.setProperty('outline', outlineStyle);
+    }
+  };
+  window.addEventListener('mousedown', detectClick);
+}());
+// Tutorial 👉 https://codyhouse.co/tutorials/immersive-section-transition-effect
+(function() {
+  var ImmerseSectionTr = function(element) {
+    this.element = element;
+    this.media = this.element.getElementsByClassName('js-immerse-section-tr__media');
+    this.scrollContent = this.element.getElementsByClassName('js-immerse-section-tr__content');
+    if(this.media.length < 1) return;
+    this.figure = this.media[0].getElementsByClassName('js-immerse-section-tr__figure');
+    if(this.figure.length < 1) return;
+    this.visibleFigure = false;
+    this.mediaScale = 1;
+    this.mediaInitHeight = 0;
+    this.elementPadding = 0;
+    this.scrollingFn = false;
+    this.scrolling = false;
+    this.active = false;
+    this.scrollDelta = 0; // amount to scroll for full-screen scaleup
+    initImmerseSectionTr(this);
+  };
+
+  function initImmerseSectionTr(element) {
+    initContainer(element);
+    resetSection(element);
+
+    // listen to resize event and reset values
+    element.element.addEventListener('update-immerse-section', function(event){
+      resetSection(element);
+    });
+
+    // detect when the element is sticky - update scale value and opacity layer 
+    var observer = new IntersectionObserver(immerseSectionTrCallback.bind(element));
+    observer.observe(element.media[0]);
+  };
+
+  function resetSection(element) {
+    getVisibleFigure(element);
+    checkEffectActive(element);
+    if(element.active) {
+      Util.removeClass(element.element, 'immerse-section-tr--disabled');
+      updateMediaHeight(element);
+      getMediaScale(element); 
+      updateMargin(element);
+      setScaleValue.bind(element)();
+    } else {
+      // reset appearance
+      Util.addClass(element.element, 'immerse-section-tr--disabled');
+      element.media[0].style = '';
+      element.scrollContent[0].style = '';
+      updateScale(element, 1);
+      updateOpacity(element, 0);
+    }
+  };
+
+  function getVisibleFigure(element) { // get visible figure element
+    element.visibleFigure = false;
+    for(var i = 0; i < element.figure.length; i++) {
+      if(window.getComputedStyle(element.figure[i]).getPropertyValue('display') != 'none') {
+        element.visibleFigure = element.figure[i];
+        break;
+      }
+    }
+  };
+
+  function updateMediaHeight(element) { // set sticky element padding/margin + height
+    element.mediaInitHeight = element.visibleFigure.offsetHeight;
+    element.scrollDelta = (window.innerHeight - element.visibleFigure.offsetHeight) > (window.innerWidth - element.visibleFigure.offsetWidth)
+      ? (window.innerHeight - element.visibleFigure.offsetHeight)/2
+      : (window.innerWidth - element.visibleFigure.offsetWidth)/2;
+    if(element.scrollDelta > window.innerHeight) element.scrollDelta = window.innerHeight;
+    if(element.scrollDelta < 200) element.scrollDelta = 200;
+    element.media[0].style.height = window.innerHeight+'px';
+    element.media[0].style.paddingTop = (window.innerHeight - element.visibleFigure.offsetHeight)/2+'px';
+    element.media[0].style.marginTop = (element.visibleFigure.offsetHeight - window.innerHeight)/2+'px';
+  };
+
+  function getMediaScale(element) { // get media final scale value
+    var scaleX = roundValue(window.innerWidth/element.visibleFigure.offsetWidth),
+      scaleY = roundValue(window.innerHeight/element.visibleFigure.offsetHeight);
+
+    element.mediaScale = Math.max(scaleX, scaleY);
+    element.elementPadding = parseInt(window.getComputedStyle(element.element).getPropertyValue('padding-top'));
+  };
+
+  function roundValue(value) {
+    return (Math.ceil(value*100)/100).toFixed(2);
+  };
+
+  function updateMargin(element) { // update distance between media and content elements
+    if(element.scrollContent.length > 0) element.scrollContent[0].style.marginTop = element.scrollDelta+'px';
+  };
+
+  function setScaleValue() { // update asset scale value
+    if(!this.active) return; // effect is not active
+    var offsetTop = (window.innerHeight - this.mediaInitHeight)/2;
+    var top = this.element.getBoundingClientRect().top + this.elementPadding;
+
+    if( top < offsetTop && top > offsetTop - this.scrollDelta) {
+      var scale = 1 + (top - offsetTop)*(1 - this.mediaScale)/this.scrollDelta;
+      updateScale(this, scale);
+      updateOpacity(this, 0);
+    } else if(top >= offsetTop) {
+      updateScale(this, 1);
+      updateOpacity(this, 0);
+    } else {
+      updateScale(this, this.mediaScale);
+      updateOpacity(this, 1.8*( offsetTop - this.scrollDelta - top)/ window.innerHeight);
+    }
+
+    this.scrolling = false;
+  };
+
+  function updateScale(element, value) { // apply new scale value
+    element.visibleFigure.style.transform = 'scale('+value+')';
+    element.visibleFigure.style.msTransform = 'scale('+value+')';
+  };
+
+  function updateOpacity(element, value) { // update layer opacity
+    element.element.style.setProperty('--immerse-section-tr-opacity', value);
+  };
+
+  function immerseSectionTrCallback(entries) { // intersectionObserver callback
+    if(entries[0].isIntersecting) {
+      if(this.scrollingFn) return; // listener for scroll event already added
+      immerseSectionTrScrollEvent(this);
+    } else {
+      if(!this.scrollingFn) return; // listener for scroll event already removed
+      window.removeEventListener('scroll', this.scrollingFn);
+      this.scrollingFn = false;
+    }
+  };
+
+  function immerseSectionTrScrollEvent(element) { // listen to scroll when asset element is inside the viewport
+    element.scrollingFn = immerseSectionTrScrolling.bind(element);
+    window.addEventListener('scroll', element.scrollingFn);
+  };
+
+  function immerseSectionTrScrolling() { // update asset scale on scroll
+    if(this.scrolling) return;
+    this.scrolling = true;
+    window.requestAnimationFrame(setScaleValue.bind(this));
+  };
+
+  function initContainer(element) {
+    // add a padding to the container to fix the collapsing-margin issue
+    if(parseInt(window.getComputedStyle(element.element).getPropertyValue('padding-top')) == 0) element.element.style.paddingTop = '1px';
+  };
+
+  function checkEffectActive(element) { //check if effect needs to be activated
+    element.active = true;
+    if(element.visibleFigure.offsetHeight >= window.innerHeight) element.active = false;
+    if( window.innerHeight - element.visibleFigure.offsetHeight >= 600) element.active = false;
+  };
+
+  //initialize the ImmerseSectionTr objects
+  var immerseSections = document.getElementsByClassName('js-immerse-section-tr'),
+    reducedMotion = Util.osHasReducedMotion(),
+    intObserverSupported = ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype);
+
+  if(immerseSections.length < 1 ) return;
+	if( !reducedMotion && intObserverSupported) {
+    var immerseSectionsArray = [];
+		for( var i = 0; i < immerseSections.length; i++) {
+      (function(i){immerseSectionsArray.push(new ImmerseSectionTr(immerseSections[i]));})(i);
+    }
+
+    if(immerseSectionsArray.length > 0) {
+      var resizingId = false,
+        customEvent = new CustomEvent('update-immerse-section');
+      
+      window.addEventListener('resize', function() {
+        clearTimeout(resizingId);
+        resizingId = setTimeout(doneResizing, 500);
+      });
+
+      function doneResizing() {
+        for( var i = 0; i < immerseSectionsArray.length; i++) {
+          (function(i){immerseSectionsArray[i].element.dispatchEvent(customEvent)})(i);
+        };
+      };
+    };
+  } else { // effect deactivated
+    for( var i = 0; i < immerseSections.length; i++) Util.addClass(immerseSections[i], 'immerse-section-tr--disabled');
+  }
+}());
